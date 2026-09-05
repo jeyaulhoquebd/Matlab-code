0ed5611e550ee56cab6948afc38ff390b3178168
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-09-06 02:00:59 +0600

    python

--- main.m
+++ main.m
@@ -15,9 +15,9 @@ A = [1 2; 3 4];
 B = [5 6; 7 8];
 
 C = A + B;          
-D = A * B;          % matrix multiplication
-E = A';             % transpose
-F = inv(A);         % inverse
+D = A * B;          
+E = A';             
+F = inv(A);         
 
 disp('Matrix A + B:');
 disp(C);
